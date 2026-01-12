afc08d267b4d18940a0d6602ea02064fe858e135
Rodri Camio <rodricamio94@gmail.com>
2026-01-12 16:18:11 -0300

    Modificamos las variables

---
+++ Python/ejercicio4.py
@@ -0,0 +1,24 @@
+# Modificar los valores de las variables
+
+# Asignamos valores iniciales a las variables
+nombre = "Programador nocturno"
+nivel_energia = 85
+tiene_capa = False
+
+# Imprimimos los valores iniciales
+print("Valores iniciales:")
+print("Nombre:", nombre)
+print("Nivel de Energía:", nivel_energia)
+print("Tiene Capa:", tiene_capa)
+
+# Modificamos los valores de las variables
+nombre = "Depurador Supremo"
+nivel_energia = 100
+tiene_capa = True
+
+# Imprimimos los valores modificados
+print("\nValores modificados:")
+print("Nombre:", nombre)
+print("Nivel de Energía:", nivel_energia)
+print("Tiene Capa:", tiene_capa)
+
