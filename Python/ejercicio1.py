09c09fe8d059dc651ca7b623a78dc87bf6a9ee32
Rodri Camio <rodricamio94@gmail.com>
2026-01-12 15:33:54 -0300

    Primeros ejercicios de python

---
+++ Python/ejercicio1.py
@@ -0,0 +1,11 @@
+# Presentarse
+# Escribe un programa en Python que solicite al usuario su nombre, edad y ciudad de residencia.
+
+# Mostrar nombre
+print("Nombre Completo: Rodrigo Camio")
+
+# Mostrar edad
+print("Edad: 31 años")
+
+# Mostrar ciudad de residencia
+print("Ciudad de Residencia: Vicente Lopez, Buenos Aires, Argentina")
---
+++ Python/ejercicio2.py
@@ -0,0 +1,15 @@
+# Variables
+
+# Definimos una variable para almacenar informacion sobre una persona
+
+# Guardamos los datos en variables
+nombre = "Rodrigo Camio"
+edad = 31
+pais = "Argentina"
+frase_favorita = "Fil. 4:13 Todo lo puedo en Cristo que me fortalece."
+
+# Imprimimos la informacion almacenada en las variables
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("País:", pais)
+print("Frase favorita:", frase_favorita)
