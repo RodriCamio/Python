88e10d2d81d4a6fb792d82d7707f2b0a1eb5d43c
Rodri Camio <rodricamio94@gmail.com>
2026-01-12 16:09:36 -0300

    tipos de variables

---
+++ Python/ejercicio3.py
@@ -0,0 +1,33 @@
+# Tipos de datos en Python
+# "Ficha secreta de super héroe"
+
+# Tipos de datos basicos en accion:
+
+# string (cadena de texto)
+nombre = "Clark Kent"
+poder_secreto = "Super fuerza"
+
+#integer (entero)
+edad = 30
+misiones_completadas = 150
+
+# float (decimal)
+nivel_energia = 99.5
+
+# boolean (booleano)
+tiene_capa = True
+es_visible = False
+
+# NoneType (tipo nulo)
+arma_secreta = None
+
+# Imprimimos la ficha del super héroe
+print("=== Ficha del Super Héroe ===")
+print("Nombre:", nombre)
+print("Poder Secreto:", poder_secreto)
+print("Edad:", edad)
+print("Misiones Completadas:", misiones_completadas)
+print("Nivel de Energía:", nivel_energia)
+print("Tiene Capa:", tiene_capa)
+print("Es Visible:", es_visible)
+print("Arma Secreta:", arma_secreta)
