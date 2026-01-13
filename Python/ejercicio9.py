3661de2185565e29406d8735e246c9d0c8378bce
Rodri Camio <rodricamio94@gmail.com>
2026-01-13 11:36:50 -0300

    Strings

---
+++ Python/ejercicio9.py
@@ -0,0 +1,14 @@
+# Cadenas o Strings
+
+# 1. comillas simples
+cadena1 = 'Manzana'
+
+# 2. comillas dobles
+cadena2 = "Banana"
+
+# ¿Por que existen dos formas de definir cadenas en Python? Para poner comillas dentro de la cadena sin necesidad de usar caracteres de escape.
+cadena3 = "Ella dijo: 'Hola'"
+cadena4 = 'Él respondió: "¿Cómo estás?"'
+
+print(cadena3) # Resultado: Ella dijo: 'Hola'
+print(cadena4) # Resultado: Él respondió: "¿Cómo estás?"
