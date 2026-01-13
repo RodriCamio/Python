5a7682eee3e52b0ae85f42f46bb87b2fa3a6108c
Rodri Camio <rodricamio94@gmail.com>
2026-01-13 11:47:14 -0300

    Caracteres especiales

---
+++ Python/ejercicio11.py
@@ -0,0 +1,17 @@
+# Manejo de caracteres especiales en cadenas de texto
+
+# \n - Nueva línea
+ejemplo_nueva_linea = "Hola,\nMundo!"
+print(ejemplo_nueva_linea)
+
+# \t - Tabulación
+ejemplo_tabulacion = "Hola,\tMundo!"
+print(ejemplo_tabulacion)
+
+# \" - Comillas dobles
+ejemplo_comillas_dobles = "Ella dijo: \"Hola, Mundo!\""
+print(ejemplo_comillas_dobles)
+
+# \' - Comillas simples
+ejemplo_comillas_simples = 'Ella dijo: \'Hola, Mundo!\''
+print(ejemplo_comillas_simples)
