8d996d4e03470093641dddd4ef495bbcdf78794c
Rodri Camio <rodricamio94@gmail.com>
2026-01-13 11:40:14 -0300

    Cadenas multilineas

---
+++ Python/ejercicio10.py
@@ -0,0 +1,24 @@
+# Usando triple comillas para definir una cadena de múltiples líneas
+# Ideal para textos largos o documentación
+
+# Ejemplo 1: un menu de restaurante
+menu = """
+Menú del Restaurante
+----------------------
+1. Ensalada César
+2. Sopa de Tomate
+3. Pollo a la Parrilla
+"""
+print(menu)
+
+# Ejemplo 2: Redactando un correo electrónico
+email = """
+Hola equipo:
+
+Les aviso que ya termine la tarea de Python.
+¡El codigo está listo para revisión!
+
+Saludos,
+Rodri Camio
+"""
+print(email)
