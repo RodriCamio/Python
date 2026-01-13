fe5b3420107f2d8d4c3e19bb0579c836d9008ff5
Rodri Camio <rodricamio94@gmail.com>
2026-01-13 11:58:05 -0300

    Manejo de la funcion len

---
+++ Python/ejercicio13.py
@@ -0,0 +1,10 @@
+# Longitud de una cadena en Python
+
+# Definir una cadena de texto
+cadena = "Hola, ¿cómo estás?"
+
+# Calcular la longitud de la cadena
+longitud = len(cadena)
+
+# Imprimir la longitud de la cadena
+print(f"La longitud de la cadena es: {longitud}")
