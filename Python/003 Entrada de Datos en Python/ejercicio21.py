38ad2e4f3f95cf8edbcee796bc2eecbe96703930
Rodri Camio <rodricamio94@gmail.com>
2026-01-14 14:40:12 -0300

    Conversion de tipos

---
+++ Python/003 Entrada de Datos en Python/ejercicio21.py
@@ -0,0 +1,11 @@
+# Conversion de tipos de datos
+
+# Definimos una variable de tipo string
+
+numero_str = "12345"
+
+total = int(numero_str) + 100
+print(f"El total es: {total}") # Imprime: El total es: 12445
+
+concatenacion = numero_str + str(67890)
+print(f"Concatenación de cadenas: {concatenacion}") # Imprime: Concatenación de cadenas: 1234567890
