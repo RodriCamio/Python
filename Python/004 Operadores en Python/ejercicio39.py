a71f23520381ccd5a353e09af7900c4e76912fc6
Rodri Camio <rodricamio94@gmail.com>
2026-01-19 12:58:52 -0300

    Sistema Prestamo de libros (OR), correcciones

--- Python/004 Operadores en Python/ejercicio39.py
+++ Python/004 Operadores en Python/ejercicio39.py
@@ -9,9 +9,9 @@
 
 print("=== Sistema Prestamo de libros ===")
 LIMITE_DISTANCIA = 3 # Definimos una constante que es el limite de km hasta donde se puede prestar el libro.
-tiene_membresia = input('¿Usted cuenta con la credencial de la biblioteca? (si/no): ') # Consultamos si el usuario tiene credencial.
+tiene_credencial = input('¿Usted cuenta con la credencial de la biblioteca? (si/no): ') # Consultamos si el usuario tiene credencial.
 cantidad_kilometros = int(input('¿A cuantos km a la redonda vives?: ')) # Consultamos al usuario a cuantos kilometros a la redonda vive.
 
-prestamo_libro = (tiene_membresia.strip().lower() == 'si') or (cantidad_kilometros <= LIMITE_DISTANCIA) # Usamos el operador OR para comparar los valores y determinar si alguno de los dos es verdadero o si ninguno lo es. 
+prestamo_libro = (tiene_credencial.strip().lower() == 'si') or (cantidad_kilometros <= LIMITE_DISTANCIA) # Usamos el operador OR para comparar los valores y determinar si alguno de los dos es verdadero o si ninguno lo es. 
 
 print(f'¿Tienes acceso al descuento VIP?: {prestamo_libro}')
