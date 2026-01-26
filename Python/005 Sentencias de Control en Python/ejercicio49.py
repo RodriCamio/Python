c5b1df58d730a7926e03be30cc2e96dbce783309
Rodri Camio <rodricamio94@gmail.com>
2026-01-26 18:58:58 -0300

    Diagrama de flujo

---
+++ Python/005 Sentencias de Control en Python/ejercicio49.py
@@ -0,0 +1,10 @@
+# Diagrama de Flujo
+
+# Un diagrama de flujo es una representacion grafica de los pasos a ejecutar para lograr un resultado especifico.
+
+# Se utilizan simbolos estandarizados para representar distintos tipos de acciones.
+
+# - Un Ovalo represnta el inicio o fin de un proceso.
+# - Un rectangulo muestra instrucciones o aaciones a ejecutar.
+# - Un rombo o diamante indica decisiones, con multiples fluje dependiendo si la respuesta es verdadera o falsa.
+# - Las flechas dirigen el flujo del proceso, mostrando la direccion en que se mueven la secuencia de acciones.
