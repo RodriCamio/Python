661a74226c6adaee69469fbbe4b69dbdb5740db6
Rodri Camio <rodricamio94@gmail.com>
2026-01-26 19:04:40 -0300

    Sentencia if else

---
+++ Python/005 Sentencias de Control en Python/ejercicio50.py
@@ -0,0 +1,19 @@
+# Sentencia if else
+
+# La sentencia else se usa para ejecutar un bloque de codigo cuando la condicion del if es falsa.
+
+# Sintaxis sentencia if else.
+# if condicion:
+    # Bloque de codigo que se ejecuta
+    # si la condicion es verdadera.
+# else:
+    # Bloque de codigo que se ejecuta
+    # si la condicion es falsa.
+
+# Ejemplo de sentencia if else:
+print('Ejemplo de Sentencia If Else: ')
+edad = int(input('Carga tu edad: '))
+if edad >= 18:
+    print('Eres mayor de edad.')
+else:
+    print('No eres mayor de edad.')
