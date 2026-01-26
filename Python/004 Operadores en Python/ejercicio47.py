62dd92760043ef4c879b2585cac61a85907482ac
Rodri Camio <rodricamio94@gmail.com>
2026-01-26 16:22:54 -0300

    Precedencia de Operadores

---
+++ Python/004 Operadores en Python/ejercicio47.py
@@ -0,0 +1,16 @@
+# Precedencia de Operadores
+
+# La precedencia de operadores determina el orden en que se evaluan las operaciones en una expresion.
+# Python aplica la siguiente tabla para asegurar que algunos operadores tengan mayor prioridad que otros durante la evaluacion de expresiones.
+# 1. Operador de parentesis ()
+# 2. Exponente **
+# 3. Unarios de Positivo (+) y Negativo (-)
+# 4. Multiplicacion, Division, Modulo *, /, //, %
+# 5. Suma y Resta +, -
+# 6. Comparacion ==, !=, <, <=, >, >=
+# 7. Operadores Logicos  Not, And y Or
+# 8. Operadores Asignacion =, +=, -=, /=, //=, **=
+
+# ejemplo = 5 + 3 * 2 ** 2 # 17
+# ejemplo_2 = ( 5 + 3 ) * 2 ** 2 # 32
+
