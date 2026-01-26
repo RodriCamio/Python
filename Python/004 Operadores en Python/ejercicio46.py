d371579c717a91440a768e3300538f37e918cb0e
Rodri Camio <rodricamio94@gmail.com>
2026-01-26 15:34:07 -0300

    Calculo Area y Perimetro de un Rectangulo

---
+++ Python/004 Operadores en Python/ejercicio46.py
@@ -0,0 +1,18 @@
+# Calculo Area y Perimetro de un Rectangulo
+
+# Se solicita calcular el area y perimetro de un rectangulo aplicando las siguientes formulas.
+# area = base * altura
+# perimetro = 2 * (base + altura)
+
+print('=== Calculo Area y Perimetro de un Rectangulo ===')
+
+# Ingresemos los valores
+BASE = int(input('Ingrese la base: '))
+ALTURA = int(input('Ingrese la altura: '))
+
+# Creamos constantes
+AREA = BASE * ALTURA
+PERIMETRO = 2 * (BASE + ALTURA)
+
+# Imprimimos los resultados
+print(f'\nEl Area del rectangulo es: {AREA}. Y el Perimetro es: {PERIMETRO}')
