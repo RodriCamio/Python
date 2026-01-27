8c80f990a34a5ae7cd35561ab11d072d1bd86c5d
Rodri Camio <rodricamio94@gmail.com>
2026-01-27 15:02:50 -0300

    sintaxis operador ternario

---
+++ Python/005 Sentencias de Control en Python/ejercicio56.py
@@ -0,0 +1,11 @@
+# Operador Ternario
+
+# El operador ternadio en Python es una forma compacta de agregar una condicion, y el objetivo es asignar un valor a una variable dependiendo del valor de la condicion.
+
+# Sintaxis Operador Ternario
+# resultaro = valor_si_verdadero if condicion else valor_si_falso
+
+# Ejemplo Operador Teernario
+edad = 18
+es_adulto = 'Si' if edad >= 18 else 'No'
+print(es_adulto) # Salida: Si
