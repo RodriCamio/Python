14d7c6fe3d642a6ae0842329440c3c438f5ee9d9
Rodri Camio <rodricamio94@gmail.com>
2026-01-27 19:08:28 -0300

    Ciclos en Python

---
+++ Python/006 Ciclos while en Python/ejercicio65.py
@@ -0,0 +1,20 @@
+# Ciclos en Python
+
+# Los Ciclos en Python son estructuras de control que repiten una serie de instrucciones hasta que se cumple una condicion especifica.
+
+# En python tenemos dos tipos de estructuras para ejecutar ciclos:
+    # - While
+    # - For
+
+# Empezamos por el While. En este ciclo se repite una serie de instrucciones mientras la condicion a evaluar sea verdadera. 
+
+# Sintaxis ciclo while:
+# while condicion:
+    # Bloque de codigo a ejecutar.
+
+# Ejemplo ciclo while
+# Imprimir de 1 a 3
+contador = 1
+while contador <= 3:
+    print(contador)
+    contador += 1
