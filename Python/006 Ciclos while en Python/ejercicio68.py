599a96fee79ba9cf789e63358c123b0654c84d5d
Rodri Camio <rodricamio94@gmail.com>
2026-02-04 15:55:43 -0300

    Sentencias de decision

---
+++ Python/006 Ciclos while en Python/ejercicio68.py
@@ -0,0 +1,13 @@
+# sentencias de decision 
+
+# Las sentencias de decision nos permiten controlar el flijo de ejecucion de un programa.
+# Las estrictiras que podemos usar son: if, else y elif.
+# - La sentencia if permite ejecutar un bloque de codigo si la condicion a evaliar es verdadera. Una condicion es una expresion que evalua a True o False, ej: edad >=18
+# Sintaxis sentencia if:
+# if condicion:
+#     bloque de codigo
+#     Ejecuta si la condicion es True
+# Ejemplo Sentencia if:
+edad = 30
+if edad >=18:
+    print('Eres mayor de edad.')
